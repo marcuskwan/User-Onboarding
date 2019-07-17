7c481e6c163ed076c3820b801d4470539974be9a
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-18 05:56:42 +1000

    added dependencies, created form component

---
+++ user-onboarding/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormC from './components/FormC'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     hi
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/FormC.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function FormC(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+FormC.propTypes = {
+
+}
+
