50f8339ac7a230241295f7bfaf3b7abbc609b78b
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-18 08:58:47 +1000

    added styling

--- user-onboarding/src/components/FormC.jsx
+++ user-onboarding/src/components/FormC.jsx
@@ -1,31 +1,91 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withFormik, Form, Field } from "formik";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { TextField, CheckboxWithLabel } from "formik-material-ui";
 import axios from "axios";
 import * as Yup from "yup";
-
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    maxWidth: "1000px",
+    margin: "0 auto",
+    minHeight: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textField: {
+    margin: "0 5px",
+  },
+  terms: {
+    marginRight: "5px",
+  },
+  left: {
+    marginLeft: "5px",
+  },
+}));
 export function FormC({ handleChange, values, errors, touched, isSubmitting }) {
+  const classes = useStyles();
   return (
     <div>
-      <Form>
-        <Field name="name" placeholder="name" onChange={handleChange} />
-        <Field name="email" placeholder="email" onChange={handleChange} />
-        <Field name="password" placeholder="password" onChange={handleChange} />
-        Terms Of Service
+      <Form className={classes.container}>
+        <Field
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+          autoComplete="off"
+          component={TextField}
+          className={classes.textField}
+        />
+        <Field
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+          autoComplete="off"
+          component={TextField}
+          className={classes.textField}
+        />
+        <Field
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+          autoComplete="off"
+          component={TextField}
+          className={classes.textField}
+          type="password"
+        />
+        <div className={classes.terms}>Terms & Conditions</div>
         <Field
           name="tos"
-          type="checkbox"
           checked={values.tos}
           onChange={handleChange}
+          autoComplete="off"
+          component={CheckboxWithLabel}
+          color="primary"
+          label="TOS"
         />
-        <button type="submit">Submit</button>
-        {isSubmitting && <p>Submitting..</p>}
+        <Button variant="outlined" type="submit" disabled={isSubmitting}>
+          Submit &rarr;
+        </Button>
+        {isSubmitting && <p className={classes.left}>Submitting..</p>}
       </Form>
     </div>
   );
 }
 
-FormC.propTypes = {};
+FormC.propTypes = {
+  handleChange: PropTypes.func,
+  values: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    password: PropTypes.string,
+    tos: PropTypes.bool,
+  }),
+  errors: PropTypes.object,
+  touched: PropTypes.object,
+  isSubmitting: PropTypes.bool,
+};
 
 export default withFormik({
   // makin props
@@ -51,11 +111,12 @@ export default withFormik({
     // checkbox: Yup.boolean().required("Terms of Service must be accepted"),
   }),
   handleSubmit(values, formikBag) {
+    formikBag.resetForm();
     const url = "https://reqres.in/api/users";
     formikBag.setSubmitting(true);
     axios.post(url, values).then(response => {
       console.log(response);
-      window.alert(`Post success! Welcome ${response.data.name} !`);
+      window.alert(`Post success! Welcome ${response.data.name}`);
       formikBag.setSubmitting(false);
     });
   },
